36ab3fa3a57aeb604eb40d07365c4b1894069b47
galronen10 <galronen10@gmail.com>
2025-07-18 15:08:59 +0300

    fix auto delete imports

--- eslint.config.js
+++ eslint.config.js
@@ -32,6 +32,18 @@ export default tseslint.config([
         typescript: {},
       },
     },
+     rules: {
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
